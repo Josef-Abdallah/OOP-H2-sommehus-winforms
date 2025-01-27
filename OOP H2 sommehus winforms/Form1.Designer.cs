cd463fb0d88c89cf2922d8ec3992876479a99bd6
Josef Larsen <josa@milestone.dk>
2025-01-27 10:44:53 +0100

    adderet knapper

--- OOP H2 sommehus winforms/Form1.Designer.cs
+++ OOP H2 sommehus winforms/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 160);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sommerhusejere";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(701, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(356, 160);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "sommerhus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1063, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(356, 160);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "resivation af sommerhus";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1713, 855);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- OOP H2 sommehus winforms/Form1.cs
+++ OOP H2 sommehus winforms/Form1.cs
@@ -16,5 +16,25 @@ namespace OOP_H2_sommehus_winforms
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
