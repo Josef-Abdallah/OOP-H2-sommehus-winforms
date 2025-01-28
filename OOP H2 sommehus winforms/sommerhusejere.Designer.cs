77fa0348bb5e3593d30ffdf9d8a50ed41e55eea4
Mustafa Shah <shahmustafa051@gmail.com>
2025-01-28 14:34:11 +0100

    update sommerhusejere

--- OOP H2 sommehus winforms/sommerhusejere.Designer.cs
+++ OOP H2 sommehus winforms/sommerhusejere.Designer.cs
@@ -38,15 +38,22 @@
             this.txt_email = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.StartsideBut = new System.Windows.Forms.Button();
+            this.txt_email_redigere = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_redigere = new System.Windows.Forms.Button();
+            this.txt_tlf_redigere = new System.Windows.Forms.TextBox();
+            this.txt_navn_redigere = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button_opret
             // 
-            this.button_opret.Location = new System.Drawing.Point(158, 270);
-            this.button_opret.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_opret.Location = new System.Drawing.Point(105, 176);
             this.button_opret.Name = "button_opret";
-            this.button_opret.Size = new System.Drawing.Size(112, 35);
+            this.button_opret.Size = new System.Drawing.Size(75, 23);
             this.button_opret.TabIndex = 23;
             this.button_opret.Text = "Opret";
             this.button_opret.UseVisualStyleBackColor = true;
@@ -54,28 +61,25 @@
             // 
             // txt_tlf
             // 
-            this.txt_tlf.Location = new System.Drawing.Point(89, 198);
-            this.txt_tlf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_tlf.Location = new System.Drawing.Point(59, 129);
             this.txt_tlf.Name = "txt_tlf";
-            this.txt_tlf.Size = new System.Drawing.Size(181, 26);
+            this.txt_tlf.Size = new System.Drawing.Size(122, 20);
             this.txt_tlf.TabIndex = 22;
             // 
             // txt_navn
             // 
-            this.txt_navn.Location = new System.Drawing.Point(89, 169);
-            this.txt_navn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_navn.Location = new System.Drawing.Point(59, 105);
             this.txt_navn.Name = "txt_navn";
-            this.txt_navn.Size = new System.Drawing.Size(181, 26);
+            this.txt_navn.Size = new System.Drawing.Size(122, 20);
             this.txt_navn.TabIndex = 21;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(18, 198);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 29);
+            this.label3.Size = new System.Drawing.Size(22, 20);
             this.label3.TabIndex = 20;
             this.label3.Text = "tlf";
             // 
@@ -83,10 +87,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 165);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(9, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.Size = new System.Drawing.Size(45, 20);
             this.label2.TabIndex = 19;
             this.label2.Text = "Navn";
             // 
@@ -94,59 +97,133 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(74, 63);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(49, 41);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(227, 47);
+            this.label4.Size = new System.Drawing.Size(149, 31);
             this.label4.TabIndex = 18;
             this.label4.Text = "Opret ejere";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(641, 546);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Location = new System.Drawing.Point(427, 355);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(846, 343);
+            this.dataGridView1.Size = new System.Drawing.Size(564, 223);
             this.dataGridView1.TabIndex = 24;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // txt_email
             // 
-            this.txt_email.Location = new System.Drawing.Point(89, 234);
-            this.txt_email.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_email.Location = new System.Drawing.Point(59, 152);
             this.txt_email.Name = "txt_email";
-            this.txt_email.Size = new System.Drawing.Size(181, 26);
+            this.txt_email.Size = new System.Drawing.Size(122, 20);
             this.txt_email.TabIndex = 28;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(18, 230);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(12, 150);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 29);
+            this.label5.Size = new System.Drawing.Size(46, 20);
             this.label5.TabIndex = 27;
             this.label5.Text = "email";
             // 
             // StartsideBut
             // 
-            this.StartsideBut.Location = new System.Drawing.Point(35, 546);
-            this.StartsideBut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartsideBut.Location = new System.Drawing.Point(23, 355);
             this.StartsideBut.Name = "StartsideBut";
-            this.StartsideBut.Size = new System.Drawing.Size(135, 60);
+            this.StartsideBut.Size = new System.Drawing.Size(90, 39);
             this.StartsideBut.TabIndex = 49;
             this.StartsideBut.Text = "Tilbage";
             this.StartsideBut.UseVisualStyleBackColor = true;
             this.StartsideBut.Click += new System.EventHandler(this.StartsideBut_Click);
             // 
+            // txt_email_redigere
+            // 
+            this.txt_email_redigere.Location = new System.Drawing.Point(286, 152);
+            this.txt_email_redigere.Name = "txt_email_redigere";
+            this.txt_email_redigere.Size = new System.Drawing.Size(122, 20);
+            this.txt_email_redigere.TabIndex = 57;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "email";
+            // 
+            // button_redigere
+            // 
+            this.button_redigere.Location = new System.Drawing.Point(332, 176);
+            this.button_redigere.Name = "button_redigere";
+            this.button_redigere.Size = new System.Drawing.Size(75, 23);
+            this.button_redigere.TabIndex = 55;
+            this.button_redigere.Text = "Redigere";
+            this.button_redigere.UseVisualStyleBackColor = true;
+            this.button_redigere.Click += new System.EventHandler(this.button_redigere_Click);
+            // 
+            // txt_tlf_redigere
+            // 
+            this.txt_tlf_redigere.Location = new System.Drawing.Point(285, 129);
+            this.txt_tlf_redigere.Name = "txt_tlf_redigere";
+            this.txt_tlf_redigere.Size = new System.Drawing.Size(122, 20);
+            this.txt_tlf_redigere.TabIndex = 54;
+            // 
+            // txt_navn_redigere
+            // 
+            this.txt_navn_redigere.Location = new System.Drawing.Point(285, 107);
+            this.txt_navn_redigere.Name = "txt_navn_redigere";
+            this.txt_navn_redigere.Size = new System.Drawing.Size(122, 20);
+            this.txt_navn_redigere.TabIndex = 53;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(239, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 20);
+            this.label6.TabIndex = 52;
+            this.label6.Text = "tlf";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(236, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 20);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "Navn";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(237, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(191, 31);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Redigere ejere";
+            // 
             // sommerhusejere
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1557, 915);
+            this.ClientSize = new System.Drawing.Size(1038, 595);
+            this.Controls.Add(this.txt_email_redigere);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_redigere);
+            this.Controls.Add(this.txt_tlf_redigere);
+            this.Controls.Add(this.txt_navn_redigere);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.StartsideBut);
             this.Controls.Add(this.txt_email);
             this.Controls.Add(this.label5);
@@ -157,7 +234,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "sommerhusejere";
             this.Text = "sommerhusejere";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -178,5 +254,13 @@
         private System.Windows.Forms.TextBox txt_email;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button StartsideBut;
+        private System.Windows.Forms.TextBox txt_email_redigere;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_redigere;
+        private System.Windows.Forms.TextBox txt_tlf_redigere;
+        private System.Windows.Forms.TextBox txt_navn_redigere;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
