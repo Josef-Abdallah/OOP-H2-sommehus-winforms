b2e3c17b4890672215e39e39738eac61fdac3bd3
Josef Larsen <josef.larsen.a@hotmail.com>
2025-01-28 12:14:06 +0100

    maximize

--- OOP H2 sommehus winforms/sommerhusejere.Designer.cs
+++ OOP H2 sommehus winforms/sommerhusejere.Designer.cs
@@ -147,6 +147,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "sommerhusejere";
             this.Text = "sommerhusejere";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
