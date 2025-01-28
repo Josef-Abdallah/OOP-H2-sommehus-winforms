e72184e94ab72f6efa205b0ca5ceb2a1cb93706b
Mustafa Shah <shahmustafa051@gmail.com>
2025-01-28 09:08:00 +0100

    chnaged the design in the form

--- OOP H2 sommehus winforms/Sommerhus.Designer.cs
+++ OOP H2 sommehus winforms/Sommerhus.Designer.cs
@@ -66,7 +66,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(324, 38);
+            this.label1.Location = new System.Drawing.Point(668, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 25);
             this.label1.TabIndex = 0;
@@ -76,12 +76,12 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(626, 526);
+            this.dataGridView1.Location = new System.Drawing.Point(644, 220);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(596, 185);
+            this.dataGridView1.Size = new System.Drawing.Size(769, 355);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -297,7 +297,7 @@
             // 
             // button_Slet
             // 
-            this.button_Slet.Location = new System.Drawing.Point(831, 427);
+            this.button_Slet.Location = new System.Drawing.Point(912, 177);
             this.button_Slet.Name = "button_Slet";
             this.button_Slet.Size = new System.Drawing.Size(75, 23);
             this.button_Slet.TabIndex = 47;
@@ -346,7 +346,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1252, 729);
+            this.ClientSize = new System.Drawing.Size(1450, 719);
             this.Controls.Add(this.StartsideBut);
             this.Controls.Add(this.button_Slet);
             this.Controls.Add(this.txt_Snavn);
